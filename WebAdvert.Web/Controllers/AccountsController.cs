1d37e4508d1ca5358ebb9157d5303c47e52cc679
amitkumar24-naga <amit.kumar24@nagarro.com>
2020-03-25 16:05:34 +0530

    first commit with login

---
+++ WebAdvert.Web/Controllers/AccountsController.cs
@@ -0,0 +1,121 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly UserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountsController(SignInManager<CognitoUser> signInManager, UserManager<CognitoUser> userManager, CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _pool = pool;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Signup()
+        {
+            var signupModel = new SignupModel();
+            return View(signupModel);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Confirm()
+        {
+            var confirmModel = new ConfirmModel();
+            return View(confirmModel);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            var loginModel = new LoginModel();
+            return View(loginModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false).ConfigureAwait(false);
+                if (user.Succeeded)
+                {
+                   return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("Invalid", "Invalid credentials!");
+                    return View(model);
+                }
+            }
+            ModelState.AddModelError("Invalid", "Invalid credentials!");
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Invalid", "Email or code is not valid!");
+                return View(model);
+            }
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user == null)
+            {
+                ModelState.AddModelError("Invalid", "Email or code is not valid!");
+                return View(model);
+            }
+            var result = await ((CognitoUserManager<CognitoUser>)_userManager).ConfirmSignUpAsync(user, model.Code, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.Code, item.Description);
+                }
+            }
+            return View(model);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("UserExists", "User with this email already exists!");
+                    return View(model);
+                }
+                //user.Attributes.Add("FirstName", model.FirstName);
+                //user.Attributes.Add("LastName", model.LastName);
+                //user.Attributes.Add("email", model.Email);
+                user.Attributes.Add("address", model.Address);
+                user.Attributes.Add("birthdate", model.DateOfirth);
+
+                var createdUser = await _userManager.CreateAsync(user, model.Password).ConfigureAwait(false);
+                if (createdUser.Succeeded)
+                {
+                   return RedirectToAction("Confirm", "Accounts");
+                }
+                else
+                {
+                    foreach (var item in createdUser.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be atleast 6 character long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "confirm password is not same as password!")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(1, ErrorMessage = "First Name cannot be empty")]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(1, ErrorMessage = "Last Name cannot be empty")]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [MinLength(1, ErrorMessage = "Address cannot be empty")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "DateOfBirth")]
+        public string DateOfirth { get; set; }
+    }
+}
